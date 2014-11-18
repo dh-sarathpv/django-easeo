3e1204cf3f701380f7fca22238f631066cd9403b
sarath pv <sarath@agaze.in>
2014-11-18 20:22:58 +0530

    first commit

---
+++ django_easeo/easeo_middleware.py
@@ -0,0 +1,29 @@
+'''
+Created on 16-Oct-2014
+
+@author: sarath
+'''
+from django.http.response import HttpResponse
+from google.appengine.api import urlfetch
+import logging
+class EaSeoMiddleWare:
+    
+    def process_request(self, request):
+        
+        try:
+            if request.GET['_escaped_fragment_']=='/':
+                
+                location = request.get_full_path()
+                absolute_uri = request.get_absolute_uri(location)
+                try:
+                    resp = urlfetch.fetch('http://2.angular-seo-dev-1001.appspot.com/service/v1/snapshot?url='+absolute_uri, method=urlfetch.GET, deadline=300)
+                    
+                    content = resp.content
+                    logging.debug(content)
+                    return HttpResponse(content)
+                except urlfetch.DownloadError:
+                    pass
+
+        
+        except:
+            pass    
